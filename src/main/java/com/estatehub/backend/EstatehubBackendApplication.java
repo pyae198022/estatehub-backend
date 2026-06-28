2b4a41bb94cfe66856b49091045de643d67796a5
pyaesone <pyaesone@pyaesones-MacBook-Air.local>
2026-06-28 22:36:01 +0630

    first building project test

---
+++ src/main/java/com/estatehub/backend/EstatehubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estatehub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatehubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatehubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estatehub/backend/EstatehubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estatehub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatehubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
